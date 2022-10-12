080040817e188c1b87957c3b0569e47c0f084106
Daniel Kim <danielkim2711@gmail.com>
2022-10-12 20:31:04 +1300

    Generate QR code

--- js/script.js
+++ js/script.js
@@ -4,11 +4,25 @@ const qr = document.getElementById('qrcode');
 const handleSubmit = (e) => {
   e.preventDefault();
 
+  // Clear QR Code first before generating another one
+  qr.innerText = '';
+
   const url = document.getElementById('url').value;
   const size = document.getElementById('size').value;
 
   showSpinner();
-  setTimeout(() => hideSpinner(), 1000);
+  setTimeout(() => {
+    hideSpinner();
+    generateQRCode(url, size);
+  }, 1000);
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size,
+  });
 };
 
 const showSpinner = () => {
